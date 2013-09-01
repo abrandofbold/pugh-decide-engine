1139020dd8649527afa4a4f72d95087ddc9c0cf4
Robert Ritter <rritter1@gmail.com>
2013-08-31 20:54:25 -0500

    Added main, null pointer exception in declaring first criteria

--- src/pde/PughDecideEngine.java
+++ src/pde/PughDecideEngine.java
@@ -32,14 +32,52 @@ public class PughDecideEngine {
 		
 		System.out.println("How many crtieria does your decision depend on?");
 		int criteriaCount = Integer.parseInt(reader.readLine());
+		System.out.println();
+		
+		ChoiceArray[] options = new ChoiceArray[optionCount];
+		System.out.println("opt len: "+options.length);
 		
 		for (int o = 0; o < optionCount; o++){
+			Criterion[] criteria = new Criterion[criteriaCount];
+			
+			System.out.print("Option Name: ");
+			String optionName = reader.readLine();
+			System.out.println();
+			
 			for (int c = 0; c < criteriaCount; c++){
+				Criterion[] criteriaSet = new Criterion[criteriaCount];
+				if (c == 0){
+					for (int cn = 0; cn < criteriaCount; cn++){
+						System.out.print("Criterion Name: ");
+						String critName = reader.readLine();
+
+						System.out.print("Weight: ");
+						float critWeight = Float.parseFloat(reader.readLine());
+						System.out.println();
+						
+						criteriaSet[cn] = new Criterion(critName,critWeight,0);
+					}
+				}
+				String name = criteriaSet[c].toString();
+				double weight = criteriaSet[c].getWeight();
 				
+				System.out.print(optionName+", "+name+" (" +weight+") "+"Value: ");
+				int value = Integer.parseInt(reader.readLine());
+				System.out.println("c: "+c);
+				
+				criteria[c] = new Criterion(name,weight,value);
 			}
+			
+			options[o] = new ChoiceArray(optionName,criteria);
+			options[o].score();
+			
+			System.out.println(options[o].toString());
 		}
 		
+		DecisionMatrix dec = new DecisionMatrix(decision,options);
+		dec.score();
 		
+		System.out.println(dec.getWinner());
 	}
 
 }
